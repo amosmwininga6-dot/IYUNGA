23d58ee31e78561ca4dbce89ce038e69218185af
amosmwininga6-dot <amosmwininga6@gmail.com>
2026-09-17 13:20:58 +0300

    Add files via upload

---
+++ Amos.js
@@ -0,0 +1,4 @@
+<button class="dark-mode-toggle" id=darkmodetoggle">togglemodetoggle</button
+const darkmodetoggle = document.getElementById('darkmodetoggle')
+darkmodetoggle.addEventListener('click' ,() =>{
+    document.body.classList.toggle('')
